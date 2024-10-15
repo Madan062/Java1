9fea3f922b3eb860cd80bbf3adf1587ae78b9baa
Madan062 <madan.23cs062@sode-edu.in>
2024-10-15 15:21:24 +0530

    Add files via upload

---
+++ M11.java
@@ -0,0 +1,10 @@
+
+public class M11 {
+    public static void main(String[] args){
+        double celcius=100;
+        double farenhite=celcius*(9.0/5.0)+32;
+        System.out.println("Temperature="+farenhite);
+                
+    }
+    
+}
