22d90da658dccad4617c0771d89896346a0c2c61
Madan062 <madan.23cs062@sode-edu.in>
2024-10-22 15:16:02 +0530

    Add files via upload

---
+++ M12.java
@@ -0,0 +1,13 @@
+
+public class M12 {
+    public static void main(String[] args){
+        System.out.println("The USN are");
+        for(int i=1;i<=9;i++){
+            System.out.println("4MW23CS00"+i);
+        }
+        for(int j=10;j<=65;j++){
+            System.out.println("4MW23CS00"+j);
+        }
+    }
+    
+}
